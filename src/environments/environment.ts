c870edb0854e0b0945a7f6db1ee3ebefaf5ab4ec
Renan Sacca <renangs2222@gmail.com>
2025-03-15 11:17:00 -0300

    inicial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyBaBbAcwYgHCejixZjog3xq5ozZdF2xspQ",
+        authDomain: "projetounimarfirebase.firebaseapp.com",
+        projectId: "projetounimarfirebase",
+        storageBucket: "projetounimarfirebase.firebasestorage.app",
+        messagingSenderId: "18371091534",
+        appId: "1:18371091534:web:d6fa4af4d0d26485b2b71f",
+
+    }
+};
