982f23633fbc999f9c02553fee1c2aa851670d30
Renan Sacca <renangs2222@gmail.com>
2025-03-15 13:36:08 -0300

    Adicionado aluno

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProfessorCrudComponent } from './pages/professor-crud/professor-crud.component';
+import { AlunoCrudComponent } from './pages/aluno-crud/aluno-crud.component';
 import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
@@ -9,4 +10,6 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'professor', component: ProfessorCrudComponent, canActivate: [AuthGuard] },
+    { path: 'aluno', component: AlunoCrudComponent, canActivate: [AuthGuard] },
+    
 ];
---
+++ src/app/interfaces/aluno.ts
@@ -0,0 +1,9 @@
+export interface Aluno {
+    nome: string,
+    cpf: string,
+    cep: string,
+    rua: string,
+    numero: string,
+    bairro: string,
+    id?: string;
+}
